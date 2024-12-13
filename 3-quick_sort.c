9fab73b0bfd821c11063840d458ebc1fd4803a6b
wlfrd18 <wilfriedpanol@gmail.com>
2024-12-13 16:06:09 +0000

    fix my for loop syntax in the quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,10 +13,10 @@
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low - 1, j;
 	int temp;
 
-	for (int j = low; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
